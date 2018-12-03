41090841f0997770509622b986ae2e153ac67443
Abigail <abigailboyer1@gmail.com>
2018-12-03 01:15:39 -0600

    fix missing bracket

--- server.js
+++ server.js
@@ -1,72 +1,60 @@
-"use strict";
-const express = require("express");
-const app = express();
-const port = 3000;
+'use strict';
+const express = require('express')
+const app = express()
+const port = 3000
 
-var mongoose = require("mongoose");
-// DB CONFIG
-const dbase = require("./config/keys").mongoUri;
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/stock');
+var MongoClient = require('mongodb').MongoClient
+var ObjectID = require('mongodb').ObjectID
 
-//CONNECT TO MOGODB
-mongoose
-  .connect(
-    dbase,
-    { useNewUrlParser: true }
-  )
-  .then(() => {
-    console.log("MONGODB Connected");
-  })
-  .catch(error => {
-    console.log(error);
-  });
-
-var request = require("request");
-app.use(express.json());
+var request = require('request');
+app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
-app.set("view engine", "pug");
-app.set("views", __dirname + "/views");
+app.use(express.static('public'));
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
 module.exports = app;
 
 var stockSchema = new mongoose.Schema({
-  Name: String,
-  Symbol: String,
-  LastPrice: Number,
-  Change: Number,
-  ChangePercent: Number,
-  MarketCap: Number,
-  Volume: Number,
-  ChangeYTD: Number,
-  ChangePercentYTD: Number,
-  High: Number,
-  Low: Number,
-  Open: Number,
-  DateCreated: {
-    type: Date,
-    default: Date.now
-  },
-  user: String
+    Name:               String,
+    Symbol:             String,
+    LastPrice:          Number,
+    Change:             Number,
+    ChangePercent:      Number,
+    MarketCap:          Number,
+    Volume:             Number,
+    ChangeYTD:          Number,
+    ChangePercentYTD:   Number,
+    High:               Number,
+    Low:                Number,
+    Open:               Number,
+    DateCreated:       {
+        type:       Date,
+        default:    Date.now
+    },
+    user : String
 });
 var favSchema = new mongoose.Schema({
-  Name: String,
-  Symbol: String,
-  LastPrice: Number,
-  Change: Number,
-  ChangePercent: Number,
-  MarketCap: Number,
-  Volume: Number,
-  ChangeYTD: Number,
-  ChangePercentYTD: Number,
-  High: Number,
-  Low: Number,
-  Open: Number,
-  DateCreated: {
-    type: Date,
-    default: Date.now
-  }
+    Name:               String,
+    Symbol:             String,
+    LastPrice:          Number,
+    Change:             Number,
+    ChangePercent:      Number,
+    MarketCap:          Number,
+    Volume:             Number,
+    ChangeYTD:          Number,
+    ChangePercentYTD:   Number,
+    High:               Number,
+    Low:                Number,
+    Open:               Number,
+    DateCreated:       {
+        type:       Date,
+        default:    Date.now
+    }
 });
 var lookupSchema = new mongoose.Schema({
-  Name: String,
+  Name:  String ,
   Symbol: String,
   Exchange: String
 });
@@ -137,13 +125,13 @@ var User = mongoose.model('User',userSchema);
 var Article = mongoose.model('MarketNews', marketNewsSchema);
 var marketCompany = mongoose.model('marketCompany', marketCompanySchema);
 
-var session = "";
-var company = {};
+var session="";
+var company={}
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-app.get('/', (req, res) => {
+app.get('/', (req, res) => {/*
   if(session===""){
   res.render('login', {user:{}})
 }
@@ -151,7 +139,7 @@ app.get('/', (req, res) => {
     res.render('index', { title: "Welcome " +session, stocks:  company })
   }
   console.log(session);
-  console.log(req.body);
+  console.log(req.body);*/
   res.render('index', {stocks:company})
 });
 
@@ -173,86 +161,72 @@ app.post('/login', (req, res) => {
       console.log(err)
       res.render('error', {})
     } else {
-      res.render("index", { title: "Welcome " + session, stocks: company });
+      if (login === null) {
+
+        res.render('error', { message: "Username or password incorrect" })
+      }else {
+      Favourites.find({}, function(err, stocks) {
+    if (err) {
+      console.log(err)
+      res.render('error', {})
+    } else {
+      session = login.Name;
+      res.render('index', { title: "Welcome " +login.Name ,  stocks:  stocks })
     }
-    console.log(session);
-    console.log(req.body);
   });
-  app.get("/api/signup", (req, res) => {
-    res.render("register", {});
-  });
-  app.get("/api/charts", (req, res) => {
-    res.render("interactivechart", {});
-  });
-  app.get("/api/logout", (req, res) => {
-    session = "";
-    res.render("login", { user: {} });
-  });
-  app.post("/login", (req, res) => {
-    console.log(req.body.email);
+  }
+}
+});
+});
+app.post('/register', (req, res) => {
+  //res.render('index', {})
+  console.log(req.body);
 
-    User.findOne(
-      { Email: req.body.email, Password: req.body.password },
-      function(err, login) {
-        if (err) {
-          console.log(err);
-          res.render("error", {});
-        } else {
-          if (login === null) {
-            res.render("error", { message: "Username or password incorrect" });
-          } else {
-            Favourites.find({}, function(err, stocks) {
-              if (err) {
-                console.log(err);
-                res.render("error", {});
-              } else {
-                session = login.Name;
-                res.render("index", {
-                  title: "Welcome " + login.Name,
-                  stocks: stocks
-                });
-              }
-            });
+      var newUser = new User(req.body);
+
+      newUser.save(function(err) {
+          if (err) {
+              throw err;
+          } else{
+
+           res.render('login',{user:req.body})
           }
-        }
-      }
-    );
-  });
-  app.post("/register", (req, res) => {
-    //res.render('index', {})
-    console.log(req.body);
-
-    var newUser = new User(req.body);
-
-    newUser.save(function(err) {
-      if (err) {
-        throw err;
-      } else {
-        res.render("login", { user: req.body });
-      }
+      });
     });
-  });
-  app.get("/home", (req, res) => {
+  app.get('/home', (req, res) => {
     Favourites.find({}, function(err, stocks) {
       if (err) {
-        console.log(err);
-        res.render("error", {});
+        console.log(err)
+        res.render('error', {})
       } else {
-        res.render("index", { stocks: stocks });
+        res.render('index', { stocks:  stocks })
       }
     });
-  });
+    });
 
-  /* search functions */
-  app.get("/search", (req, res) => {
-    res.render("search", { title: "Search", query: {} });
-    console.log("rendering search");
-  });
+    /* search functions */
+    app.get('/search', (req, res) => {
+      res.render('search', {title:"Search", query:{} });
+      console.log("rendering search");
+    });
 
-  app.get("/api/search", (req, res) => {
-    res.render("search", { title: "Search", query: {} });
-    console.log("rendering search");
-  });
+    app.get('/api/search', (req, res) => {
+      res.render('search', {title:"Search", query:{} });
+      console.log("rendering search");
+    });
+
+    /* view the financial reports for a particular company */
+    /* with parameters for searching different ranges */
+    app.get('/stock/financials', (req, res) => {
+      res.render('Financials', {title:"financials", query:{} });
+      console.log("rendering full page financials");
+    });
+
+    /* show market stats for certain ranges */
+    app.get('/stock/market', (req, res) => {
+      res.render('market', {title:"market", query:{} });
+      console.log("rendering market stats");
+    });
 
     /* show news on market */
     app.get('/news', (req, res) => {
@@ -260,29 +234,29 @@ app.post('/login', (req, res) => {
       console.log("loading news");
     });
 
-    /* for deletion probably */
-    app.get("/stock/financials", (req, res) => {
-      res.render("Financials", { title: "financials", query: {} });
-      console.log("rendering full page financials");
+    /* get news articles for entire market  */
+    app.get('/api/marketNews', function(req,  res) {
+      var query = {
+          'symbol': req.body.id
+      };
+
+      var options = {
+          url: 'https://api.iextrading.com/1.0/stock/market/news/last/10',
+          method: 'GET',
+          qs: query
+      };
+
+      request(options, function(err, request, body) {
+          var jsonBody = JSON.parse(body);
+          var articles = jsonBody.map(function(data) {
+            return new Article(data);
+          });
+          console.log(jsonBody.length);
+          console.log(articles.length);
+
+          res.render('news', {Article:articles});
+      });
     });
-
-    /* show market stats for certain ranges */
-    app.get("/stock/market", (req, res) => {
-      res.render("market", { title: "market", query: {} });
-      console.log("rendering market stats");
-    });
-
-  /* get news articles for entire market  */
-  app.get("/api/marketNews", function(req, res) {
-    var query = {
-      symbol: req.body.id
-    };
-
-    var options = {
-      url: "https://api.iextrading.com/1.0/stock/market/news/last/10",
-      method: "GET",
-      qs: query
-    };
 
     /* show news for a single market */
     app.post('/api/coNews/', function(req,  res) {
@@ -293,9 +267,10 @@ app.post('/login', (req, res) => {
       var thisCompany = req.body.id;
     //  console.log(thisCompany);
 
-    var thisCompany = req.body.id;
-    console.log(thisCompany);
-
+      var options = {
+        url: 'https://api.iextrading.com/1.0/stock/' + thisCompany + '/news/last/20',
+        method: 'GET'
+      };
       //console.log(options)
 
       request(options, function(err, request, body) {
@@ -309,15 +284,10 @@ app.post('/login', (req, res) => {
           return new Article(data);
         });
 
-      var articles = jsonBody.map(function(data) {
-        console.log(articles);
-        return new Article(data);
+        console.log("ARTICLES: " + {Article:articles});
+        res.render('news', {Article:articles});
       });
-
-      console.log("ARTICLES: " + { Article: articles });
-      res.render("news", { Article: articles });
     });
-  });
 
   app.get('/api/markets', function(req, res) {
     res.render('markets');
@@ -342,7 +312,7 @@ app.post('/login', (req, res) => {
   });
 
   app.get('/api/markets/lose', function(req, res) {
-    /* top losing */
+    /* loseing */
     var options = {
         url: 'https://api.iextrading.com/1.0/stock/market/list/losers',
         method: 'GET'
@@ -360,7 +330,7 @@ app.post('/login', (req, res) => {
   });
 
   app.get('/api/markets/focus', function(req, res) {
-    /* focus */
+    /* in focus */
     var options = {
         url: 'https://api.iextrading.com/1.0/stock/market/list/infocus',
         method: 'GET'
@@ -373,12 +343,11 @@ app.post('/login', (req, res) => {
         });
         console.log(company.length);
 
-        res.render('markets', {marketCompany:company, title:"In Focus"});
+        res.render('markets', {marketCompany:company, title:"Focus"});
     });
   });
 
 app.post('/api/stock', function(req, res) {
-
        var query = {
            'symbol': req.body.id
        };
@@ -450,7 +419,7 @@ app.post('/api/stock', function(req, res) {
                   //remove next line and uncomment above
                   //res.render('lookup-detail',{query:jsonBody})
               }
-        });
+          });
       });
 
 app.get('/api/stock', (req, res) => {
@@ -467,37 +436,12 @@ app.get('/api/history', (req, res) => {
       res.render('history', { stocks:  stocks })
     }
   });
-});
+  });
 
 app.get('/api/lookup', (req, res) => {
   res.render('look-up', { title: "Look Up", query: {} })
   console.log(req.body.id);
-
-        newStocks.save(function(err) {
-          if (err) {
-            throw err;
-          } else {
-            console.log(jsonBody);
-            res.render("landingpage", { company: newStocks });
-          }
-        });
-        //   res.render('landingpage',{company:newStocks})
-      }
-    });
-  });
-  app.post("/api/lookup", function(req, res) {
-    var query = {
-      input: req.body.id
-    };
-
-    var options = {
-      url: "http://dev.markitondemand.com/MODApis/Api/v2/Lookup/json",
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      qs: query
-    };
+});
 
 app.get('/stock/new/:Symbol', (req, res) => {
   Stock.findOne({"symbol": req.params.Symbol}, function(err, stocks) {
@@ -509,24 +453,19 @@ app.get('/stock/new/:Symbol', (req, res) => {
       if (stocks === null) {
         res.render('error', { message: "Not found" })
       } else {
-        console.log(stocks);
-        if (stocks === null) {
-          res.render("error", { message: "Not found" });
-        } else {
-          // res.status(200).send(book)
-          // res.render('index', { stocks: stocks})
-          var fav = new Favourites(stocks);
-          fav.save(function(err) {
+       // res.status(200).send(book)
+        // res.render('index', { stocks: stocks})
+        var fav = new Favourites(stocks);
+        fav.save(function(err) {
             if (err) {
-              throw err;
-            } else {
+                throw err;
+            } else{
               console.log(jsonBody);
-              res.render("index", { stocks: stocks });
+              res.render('index',{stocks:stocks})
             }
-          });
-        }
+        });
       }
-    });
+    }
   });
 });
 
@@ -539,33 +478,33 @@ app.get('/stock/:symbol', (req, res) => {
       url: 'http://dev.markitondemand.com/MODApis/Api/v2/Quote/json',
       method: 'POST',
       headers: {
-        "Content-Type": "application/json"
+          'Content-Type': 'application/json'
       },
       qs: query
-    };
+  }
 
-    request(options, function(err, request, body) {
+  request(options, function(err, request, body) {
       // markitondemand return status 200 whether if found stock or not
       // if it found stock there will not be a message field
       // if found stock then and only then save data to MongoDB
       console.log("inside");
       var jsonBody = JSON.parse(body);
       if (!jsonBody.Message) {
-        jsonBody.user = session;
-        var newStocks = new Stock(jsonBody);
+          jsonBody.user=session;
+          var newStocks = new Stock(jsonBody);
 
-        newStocks.save(function(err) {
-          if (err) {
-            throw err;
-          } else {
-            console.log(jsonBody);
-            res.render("landingpage", { company: newStocks });
-          }
-        });
+          newStocks.save(function(err) {
+              if (err) {
+                  throw err;
+              } else{
+                console.log(jsonBody);
+                res.render('landingpage',{company:newStocks})
+              }
+          });
         //  res.render('landingpage',{company:newStocks})
       }
-    });
   });
-});
 
+});
+});
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
