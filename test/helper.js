c5aead08be8569019c52ddf3220867b112b6978b
Jarronb <baileyjarron@gmail.com>
2018-12-04 04:11:27 -0600

    travis

--- test/helper.js
+++ test/helper.js
@@ -3,8 +3,18 @@ let chaiHttp = require("chai-http");
 var app = require("../server.js");
 let server = require("../server");
 let should = chai.should();
+let assert = require("assert");
 
 // global.app = app;
 // global.expect = chai.expect;
 
 // chai.use(chaiHttp);
+describe("pow", function() {
+  it("raises to n-th power", function() {
+    assert.equal(pow(2, 3), 8);
+  });
+});
+
+function pow() {
+  return 8; // :) we cheat!
+}
