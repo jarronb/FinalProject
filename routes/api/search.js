a05865d8de6f9a01d334a8891f233f739053374c
Jarron <baileyjarron@gmail.com>
2019-03-28 02:35:22 -0500

    Formated search.js

--- routes/api/search.js
+++ routes/api/search.js
@@ -1,47 +1,45 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-var request = require("request");
+var request = require('request');
 
-var Company = require("../../models/LookUpSchema");
+var Company = require('../../models/LookUpSchema');
 
 // Load Models
-var Stock = require("../../models/Stock");
-var Favourites = require("../../models/FavStock");
+var Stock = require('../../models/Stock');
+var Favourites = require('../../models/FavStock');
 
 // Route: api/search
 
-const { ensureAuthenticated } = require("../../helpers/auth");
+const { ensureAuthenticated } = require('../../helpers/auth');
 
 /* search functions */
-router.get("/", ensureAuthenticated, (req, res) => {
-  res.render("search", { title: "Search", query: {} });
+router.get('/', ensureAuthenticated, (req, res) => {
+  res.render('search', { title: 'Search', query: {} });
 });
 
 /* show news for a single market */
-router.post("/company/news", function(req, res) {
+router.post('/company/news', function(req, res) {
   var query = {
     input: req.body.id
   };
 
   var thisCompany = req.body.id;
 
-
   var options = {
     url:
-      "https://api.iextrading.com/1.0/stock/" + thisCompany + "/news/last/20",
-    method: "GET"
+      'https://api.iextrading.com/1.0/stock/' + thisCompany + '/news/last/20',
+    method: 'GET'
   };
 
   request(options, function(err, request, body) {
-
     var jsonBody = JSON.parse(body);
 
     var articles = jsonBody.map(function(data) {
       return new Article(data);
     });
 
-    res.render("news", { Article: articles });
+    res.render('news', { Article: articles });
   });
 });
 
@@ -49,20 +47,20 @@ router.post("/company/news", function(req, res) {
 // @desc    Display search view
 //@view     search-stock
 // @access  Public
-router.get("/stock", ensureAuthenticated, (req, res) => {
-  res.render("search-stock", { title: "Search Stock", query: {} });
+router.get('/stock', ensureAuthenticated, (req, res) => {
+  res.render('search-stock', { title: 'Search Stock', query: {} });
 });
 
-router.get("/stock/:symbol", (req, res) => {
+router.get('/stock/:symbol', (req, res) => {
   var query = {
     symbol: req.params.symbol
   };
 
   var options = {
-    url: "http://dev.markitondemand.com/MODApis/Api/v2/Quote/json",
-    method: "POST",
+    url: 'http://dev.markitondemand.com/MODApis/Api/v2/Quote/json',
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json"
+      'Content-Type': 'application/json'
     },
     qs: query
   };
@@ -75,11 +73,11 @@ router.get("/stock/:symbol", (req, res) => {
     if (!jsonBody.Message) {
       var newStock = new Stock(jsonBody);
       newStock.user = req.user;
-      newStock.save(err, newStock => {
+      newStock.save(err, (newStock) => {
         if (err) {
           throw err;
         } else {
-          res.render("landingpage", { company: newStock });
+          res.render('landingpage', { company: newStock });
         }
       });
       //  res.render('landingpage',{company:newStocks})
@@ -87,16 +85,16 @@ router.get("/stock/:symbol", (req, res) => {
   });
 });
 
-router.post("/stock", function(req, res) {
+router.post('/stock', function(req, res) {
   var query = {
     input: req.body.id
   };
 
   var options = {
-    url: "http://dev.markitondemand.com/MODApis/Api/v2/Lookup/json",
-    method: "POST",
+    url: 'http://dev.markitondemand.com/MODApis/Api/v2/Lookup/json',
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json"
+      'Content-Type': 'application/json'
     },
     qs: query
   };
@@ -113,7 +111,7 @@ router.post("/stock", function(req, res) {
         if (err) {
           throw err;
         } else {
-          res.render("lookup-detail", { query: jsonBody });
+          res.render('lookup-detail', { query: jsonBody });
         }
       });
       //remove next line and uncomment above
